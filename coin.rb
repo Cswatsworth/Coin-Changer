f94faa67f366061da8d65c41933140bfb4c7fd30
Chad Swatsworth <Chad Swatsworth>
2017-02-15 20:49:07 -0500

    tests are good. starting app.

--- coin.rb
+++ coin.rb
@@ -3,7 +3,7 @@ def change(amount)
 	exact_change = []
 	[25, 10, 5, 1].each do |coin|
 		exact_change << [coin] * (amount / coin) if amount >= coin
-		amount = amount - (coin * (amount/coin))
+		amount = amount - (coin * (amount / coin))
 	end
 	exact_change.flatten
 end
---
+++ coin_app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require_relative 'coin.rb'
+
+
+get '/' do
+
+erb :change_input, :locals => {:amount => '', :results => '', :message_one => '', :number => ''}  #:message_one => "is a"
+end
+
+post '/amount' do
+	amount = params[:change_input]
+
+results = change(amount)
+
+erb :change_input, :locals => {:amount => amount, :results => results, :message_one => " is the", :number => ' change '} 
+
+end
