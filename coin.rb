ff74dcad8776ffbf4a33cbf0130c2de74d298d1d
Chad Swatsworth <Chad Swatsworth>
2017-02-15 19:35:50 -0500

    working

--- coin.rb
+++ coin.rb
@@ -1,47 +1,9 @@
-def coin_changer(cents)
-	change = {}
+def change(amount)
 
-	coin_value = {quarter: 25, dime: 10, nickle:5, penny: 1}
-
-	if cents == 0
-		return change
+	exact_change = []
+	[25, 10, 5, 1].each do |coin|
+		exact_change << [coin] * (amount / coin) if amount >= coin
+		amount = amount - (coin * (amount/coin))
 	end
-
-	if cents >= 1
-			return {:penny=>1}
-	end
-
-	if cents >= 4
-			return {:penny=>4}
-	end
-	
-	# if cents = 5
-	# 		return {:nickle=>1}
-	# end
-
-	change
+	exact_change.flatten
 end
-
-
-
-# def coin_changer(cents)
-# 	change = {}
-
-# 	coin_values = {quarter: 25, dime: 10, nickle: 5, penny: 1}
-	
-# 	coin_values.each do |coin, value|
-		
-# 		if cents >= coin_values
-# 			coins = cents / coin_value
-# 			change[coins] = coins
-# 			cents = cents - (value * coins)
-# 		end 
-
-# 	end
-# 	change
-# end
-
-
-	
-
-# 	
--- test_coin.rb
+++ test_coin.rb
@@ -8,17 +8,17 @@ class TestCoinChanger < Minitest::Test
 	end
 
 	def test_1_cent_returns_penny
-		results = coin_changer(1)
-		assert_equal({penny: 1}, results)
+		amount = 1
+		assert_equal([1], change(amount))
 	end
 
-	def test_4_cents_returns_4_pennies
-		results = coin_changer(4)
-		assert_equal({penny:1}, results)
-	end
+	def test_5_cents_returns_one_nickle
+		amount = 5
+		assert_equal([5], change(amount))
+	end		
 
-# 	def test_5_cents_returns_one_nickle
-# 		results = coin_changer(5)
-# 		assert_equal({nickle: 1}, results)
-# 	end
+	def test_7_cents_returns_1N_2P
+		amount = 7
+		assert_equal([5, 1, 1], change(amount))
+	end
 end
